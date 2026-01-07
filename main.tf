d9b1947b6b9769579b0c95e90e72f4c872341086
root <root@HOME-PC.localdomain>
2026-01-07 13:55:07 +0300

    providers

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.177.0"
-    }
-  }
-
-  required_version = ">= 1.3.0"
-}
-
-provider "yandex" {
-  service_account_key_file = var.sa_key_path
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.zone
-}
 
 module "vpc" {
   source = "./modules/vpc"
